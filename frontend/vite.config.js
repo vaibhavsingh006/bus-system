1608c8e39291eb0b8a403f3f7aa1d8475378532e
Vaibhav Singh <122105010+vaibhavsingh006@users.noreply.github.com>
2025-06-19 14:45:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects",
+          dest: ".", // copy to dist/
+        },
+      ],
+    }),
   ],
-  base: './'
-})
+  base: "./",
+});
